a5e44d9bd4ea04e36804f74fdf719a0af03fdaca
Paula Muir <paula@puppet.com>
2019-02-27 15:21:41 +0000

    change osfamily to release apache_parameters_spec.rb

--- spec/acceptance/apache_parameters_spec.rb
+++ spec/acceptance/apache_parameters_spec.rb
@@ -35,9 +35,9 @@ describe 'apache parameters' do
 
     describe service($service_name) do
       it { is_expected.to be_running }
-      if os[:family] == 'debian' && os[:family].to_i == '8'
+      if os[:family] == 'debian' && os[:release].to_i == '8'
         pending 'Should be enabled - Bug 760616 on Debian 8'
-      elsif os[:family] == 'sles' && os[:family].to_i == '15'
+      elsif os[:family] == 'sles' && os[:release].to_i == '15'
         pending 'Should be enabled - MODULES-8379 `be_enabled` check does not currently work for apache2 on SLES 15'
       else
         it { is_expected.to be_enabled }
